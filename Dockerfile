d921edaa6cc551bf6a20b5b144a7260a0d1662ff
domkaa <rutkauskasdomantas@gmail.com>
2025-05-14 21:10:50 +0300

    small fix to frontend

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,6 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 
-ARG VITE_API_URL
-ARG VITE_WS_URL
-
-ENV VITE_API_URL=${VITE_API_URL}
-ENV VITE_WS_URL=${VITE_WS_URL}
-
 RUN npm run build
 
 RUN ls -la && mkdir -p dist
--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,10 @@ export default defineConfig(({ mode }) => {
       tailwindcss()
     ],
     define: {
-      'import.meta.env.VITE_MAILTRAP_TOKEN': JSON.stringify(env.VITE_MAILTRAP_TOKEN),
       'import.meta.env.VITE_EMAIL_FROM': JSON.stringify(env.VITE_EMAIL_FROM),
       'import.meta.env.VITE_EMAIL_FROM_NAME': JSON.stringify(env.VITE_EMAIL_FROM_NAME),
-      'import.meta.env.VITE_SESSION_TIMEOUT': JSON.stringify(env.VITE_SESSION_TIMEOUT)
+      'import.meta.env.VITE_SESSION_TIMEOUT': JSON.stringify(env.VITE_SESSION_TIMEOUT),
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
     }
   }
 })
